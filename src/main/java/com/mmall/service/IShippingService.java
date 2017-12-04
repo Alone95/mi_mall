5de80361cddf14e03073af58336c6b86b61de98f
Alone95 <460454903@qq.com>
2017-12-04 23:13:11 +0800

    shipping_module

---
+++ src/main/java/com/mmall/service/IShippingService.java
@@ -0,0 +1,22 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+
+/**
+ * @author alone95
+ * @date 2017/12/4
+ */
+public interface IShippingService {
+
+    ServerResponse add(Integer userId, Shipping shipping);
+
+    ServerResponse<String> del(Integer userId,Integer shippingId);
+
+    ServerResponse update(Integer userId,Shipping shipping);
+
+    ServerResponse<Shipping> select(Integer userId,Integer shippingId);
+
+    ServerResponse<PageInfo> list(Integer userId,int pageNum,int pageSize);
+}
